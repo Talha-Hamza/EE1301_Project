1f959806fa7c3db7105ec56c75ec05c597a8b8e2
Shivam Vashishtha <58797316+ShivamVashishtha@users.noreply.github.com>
2023-11-29 18:10:48 -0600

    Add files via upload
    nov29 updated src file

---
+++ final1.cpp
@@ -0,0 +1,226 @@
+#include "Particle.h"
+#include "HC-SR04.h"
+
+SYSTEM_MODE(AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+// Right Photon - Outdoor with 2 HC-SR04 sensors and TMP36 sensor
+int rightTrigPin1 = A0;
+int rightEchoPin1 = D0;
+int rightTrigPin2 = A1;
+int rightEchoPin2 = D1;
+int tmp36Pin = A4;
+
+double distCm1 = 0.0;
+double distIn1 = 0.0;
+double distCm2 = 0.0;
+double distIn2 = 0.0;
+double tempC = 0.0;
+
+void waitForEcho(int pin, int value, long timeout);
+void sendTriggerPulse(int pin);
+double getDistanceInch(int trigPin, int echoPin);
+double getDistanceCM(int trigPin, int echoPin);
+double getTemperature(int tmp36Pin);
+
+void motionHandler(const char *event, const char *data);
+void triggerPackageAlert();
+void triggerPersonAlert();
+
+bool rightPackageDetected = false;
+bool motionDetected = false;
+unsigned long motionAlertDuration = 0;
+
+void setup()
+{
+    pinMode(rightTrigPin1, OUTPUT);
+    pinMode(rightEchoPin1, INPUT);
+    pinMode(rightTrigPin2, OUTPUT);
+    pinMode(rightEchoPin2, INPUT);
+    pinMode(tmp36Pin, INPUT);
+
+    Particle.subscribe("motion", motionHandler, MY_DEVICES);
+}
+
+void loop()
+{
+    distCm1 = getDistanceCM(rightTrigPin1, rightEchoPin1);
+    distIn1 = getDistanceInch(rightTrigPin1, rightEchoPin1);
+
+    distCm2 = getDistanceCM(rightTrigPin2, rightEchoPin2);
+    distIn2 = getDistanceInch(rightTrigPin2, rightEchoPin2);
+
+    tempC = getTemperature(tmp36Pin);
+
+    if (distCm1 < 50.0 && distCm2 < 50.0)
+    {
+        rightPackageDetected = true;
+        motionDetected = false;
+        motionAlertDuration = 0;
+    }
+    else
+    {
+        rightPackageDetected = false;
+    }
+
+    if (rightPackageDetected)
+    {
+        triggerPackageAlert();
+    }
+    else if (motionDetected)
+    {
+        triggerPersonAlert();
+    }
+
+    if (tempC > 0.0)
+    {
+        Particle.publish("outdoor_conditions", "High temperature detected");
+    }
+
+    delay(1000);
+}
+
+double getDistanceCM(int trigPin, int echoPin)
+{
+    sendTriggerPulse(trigPin);
+    waitForEcho(echoPin, HIGH, 100);
+    long startTime = micros();
+    waitForEcho(echoPin, LOW, 100);
+    long endTime = micros();
+    long duration = endTime - startTime;
+    double distance = duration / 29.0 / 2.0;
+    if (distance < 0.0 || distance > 200.0)
+    {
+        return -1;
+    }
+    return distance;
+}
+
+double getDistanceInch(int trigPin, int echoPin)
+{
+    double distCM = getDistanceCM(trigPin, echoPin);
+    if (distCM == -1)
+    {
+        return -1;
+    }
+    return (distCM / 2.54);
+}
+
+void sendTriggerPulse(int pin)
+{
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(pin, LOW);
+}
+
+void waitForEcho(int pin, int value, long timeout)
+{
+    long giveupTime = millis() + timeout;
+    while (digitalRead(pin) != value && millis() < giveupTime)
+    {
+    }
+}
+
+double getTemperature(int tmp36Pin)
+{
+    int ADCreading = analogRead(tmp36Pin);
+    double tempCelsius = (ADCreading - 620) * 0.0806;
+    return tempCelsius;
+}
+
+void motionHandler(const char *event, const char *data)
+{
+    motionDetected = true;
+    motionAlertDuration = millis();
+}
+
+void triggerPackageAlert()
+{
+    Particle.publish("package_alert", "Package detected");
+}
+
+void triggerPersonAlert()
+{
+    Particle.publish("person_alert", "Person detected");
+}
+
+// Left Photon - Outdoor with 2 HC-SR04 sensor
+int leftTrigPin1 = A0;
+int leftEchoPin1 = D0;
+int leftTrigPin2 = A1;
+int leftEchoPin2 = D1;
+
+double leftDistCm1 = 0.0;
+double leftDistIn1 = 0.0;
+double leftDistCm2 = 0.0;
+double leftDistIn2 = 0.0;
+
+void setupLeftPhoton()
+{
+    pinMode(leftTrigPin1, OUTPUT);
+    pinMode(leftEchoPin1, INPUT);
+    pinMode(leftTrigPin2, OUTPUT);
+    pinMode(leftEchoPin2, INPUT);
+}
+
+void loopLeftPhoton()
+{
+    leftDistCm1 = getDistanceCM(leftTrigPin1, leftEchoPin1);
+    leftDistIn1 = getDistanceInch(leftTrigPin1, leftEchoPin1);
+
+    leftDistCm2 = getDistanceCM(leftTrigPin2, leftEchoPin2);
+    leftDistIn2 = getDistanceInch(leftTrigPin2, leftEchoPin2);
+
+    Particle.publish("Left-Distance-CM-Sensor1", String(leftDistCm1));
+    Particle.publish("Left-Distance-IN-Sensor1", String(leftDistIn1));
+
+    Particle.publish("Left-Distance-CM-Sensor2", String(leftDistCm2));
+    Particle.publish("Left-Distance-IN-Sensor2", String(leftDistIn2));
+
+    Serial.print("Left-Distance-CM-Sensor1: ");
+    Serial.println(leftDistCm1);
+    Serial.print("Left-Distance-IN-Sensor1: ");
+    Serial.println(leftDistIn1);
+
+    Serial.print("Left-Distance-CM-Sensor2: ");
+    Serial.println(leftDistCm2);
+    Serial.print("Left-Distance-IN-Sensor2: ");
+    Serial.println(leftDistIn2);
+
+    delay(1000);
+}
+
+// Inside Photon - Indoor with Buzzer
+int insideBuzzerPin = A1;
+
+void setupInsidePhoton()
+{
+    pinMode(insideBuzzerPin, OUTPUT);
+}
+
+void loopInsidePhoton()
+{
+//what do we write here?
+    delay(1000);
+}
+
+void triggerInsidePackageAlert()
+{
+    digitalWrite(insideBuzzerPin, HIGH);
+    Particle.publish("inside_package_alert", "Package detected");
+    delay(5000); // we can adjust alert duration as needed
+    digitalWrite(insideBuzzerPin, LOW);
+}
+
+void setup()
+{
+    setupLeftPhoton();
+    setupInsidePhoton();
+}
+
+void loop()
+{
+    loopLeftPhoton();
+    loopInsidePhoton();
+}
