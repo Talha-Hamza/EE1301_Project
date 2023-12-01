3bf40693602448af33b2843bce507f9a38b32f5f
Shivam Vashishtha <58797316+ShivamVashishtha@users.noreply.github.com>
2023-12-01 11:36:01 -0600

    Create inside_photon.cpp

---
+++ inside_photon.cpp
@@ -0,0 +1,14 @@
+#include "Particle.h"
+
+int insideBuzzerPin = A1;
+
+void setup()
+{
+    pinMode(insideBuzzerPin, OUTPUT);
+}
+
+void loop()
+{
+    // Inside Photon logic here
+    delay(1000);
+}
